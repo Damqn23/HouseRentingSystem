f566e86e51f2e12d6f62adad63ed12bbbcfa744f
Damqn23 <rauga2005@gmail.com>
2024-07-05 10:51:29 +0300

    Repository added

---
+++ HouseRentingSystem.Core/Services/House/HouseService.cs
@@ -0,0 +1,37 @@
+﻿using HouseRentingSystem.Core.Contract.House;
+using HouseRentingSystem.Core.Models.Home;
+using HouseRentingSystem.Infrastucture.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentingSystem.Core.Services.House
+{
+	public class HouseService : IHouseService
+	{
+		private readonly IRepository repository;
+
+        public HouseService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+        public async Task<IEnumerable<HouseIndexServiceModel>> LastThreeHouses()
+		{
+			return await repository
+				.AllReadOnly<Infrastucture.Data.Models.House>()
+				.OrderByDescending(h => h.Id)
+				.Take(3)
+				.Select(h => new HouseIndexServiceModel()
+				{
+					Id = h.Id,
+					ImageUrl = h.ImageUrl,
+					Title = h.Title
+				}).ToListAsync();
+
+		}
+	}
+}
